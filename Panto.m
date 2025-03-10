d87a75f4caecbddf85513f9a44de54c063698778
Modska <122678542+Modska@users.noreply.github.com>
2025-03-10 15:32:13 +0100

    Sortie tableau vers commande

--- Panto.m
+++ Panto.m
@@ -1,95 +1,36 @@
-% 5 BAR PARALLEL ROBOT
-%%
-clear all; clc;
-a1=0;
-a2=0.2; a3=0.35; a4=a2; a5=a3; a32=a3; a3D=2*a3;
+% Robot dimensions
+l_or_mg = 15; % Distance between origin and left motor
+l_or_md = 120; % Distance between origin and right motor
+l1 = 240; % Length from motor to passive joints
+l2 = 220; % Length from passive joints to end effector
 
-%%
-% Forward kinematics of planar manipulandum
-syms a1 a2 a3 a4 a5 th2 th5 Cx Cy
-eq1=((Cx-a2*cos(th2))^2)+((Cy-a2*sin(th2))^2)-a3^2;
-eq2=((a5*cos(th5)-Cx)^2)+((Cy-a5*sin(th5))^2)-a4^2;
+% Square trajectory corners
+x_corners = [120, 200, 200, 120];
+y_corners = [300, 300, 400, 400];
 
-S=solve([eq1;eq2],[Cx;Cy]);
-pretty(simplify(S.Cx))
+% Calculate angles for each corner of the square trajectory
+for k = 1:length(x_corners)
+    x = x_corners(k);
+    y = y_corners(k);
 
-%% Inverse kinematics of planar manipulandum
-clear all; clc;
-syms a1 a2 a3 a32 a3D a4 a5 th2 th5 XD YD
+    % Calculate angles
+    beta1 = atan2(y, (l_or_mg));
+    beta2 = atan2(y, l_or_md);
 
-eq1=XD-a2*cos(th2)-a3D*cos(th5);
-eq2=YD-a2*sin(th2)-a3D*sin(th5);
-S=solve([eq1;eq2],[th2;th5]);
+    alpha1_calc = (l1^2 + (l_or_mg^2 + y^2) - l2^2) / (2 * l1 * sqrt(l_or_mg^2 + y^2));
+    alpha2_calc = (l1^2 + (l_or_md^2 + y^2) - l2^2) / (2 * l1 * sqrt(l_or_md^2 + y^2));
 
-%% Simulation with inverse kinematics
-clear all, clc
-a1=0;
-a2=0.2; a3=0.35; a4=a2; a5=a3; a32=a3; a3D=2*a3;
+    if alpha1_calc > 1 || alpha2_calc > 1
+        warning('Unreachable coordinates');
+        continue;
+    end
 
-t=0:0.001:10;
-YD=0.55+0.15*sin(2*pi*(1/5)*t);
-XD=0.45*ones(1,length(t));
+    alpha1 = acos(alpha1_calc);
+    alpha2 = acos(alpha2_calc);
 
-% load data_fw2inv
+    shoulder1 = beta1 + alpha1;
+    shoulder2 = -(pi - beta2 - alpha2);
 
-% th2_1=2*atan2((2*YD*a2 - (- XD.^4 - 2*XD.^2.*YD.^2 + 2*XD.^2*a2^2 + 2*XD.^2*a3D^2 - YD.^4 + 2*YD.^2*a2^2 + 2*YD.^2*a3D^2 - a2^4 + 2*a2^2*a3D^2 - a3D^4).^(1/2)),(XD.^2 + 2*XD*a2 + YD.^2 + a2^2 - a3D^2));
-th2_2=2*atan2((2*YD*a2 + (- XD.^4 - 2*XD.^2.*YD.^2 + 2*XD.^2*a2^2 + 2*XD.^2*a3D^2 - YD.^4 + 2*YD.^2*a2^2 + 2*YD.^2*a3D^2 - a2^4 + 2*a2^2*a3D^2 - a3D^4).^(1/2)),(XD.^2 + 2*XD*a2 + YD.^2 + a2^2 - a3D^2));
-% th5_1=2*atan2((2*YD*a3D + ((- XD.^2 - YD.^2 + a2^2 + 2*a2*a3D + a3D^2).*(XD.^2 + YD.^2 - a2^2 + 2*a2*a3D - a3D^2)).^(1/2)),(XD.^2 + 2*XD*a3D + YD.^2 - a2^2 + a3D^2));
-th5_2=2*atan2((2*YD*a3D - ((- XD.^2 - YD.^2 + a2^2 + 2*a2*a3D + a3D^2).*(XD.^2 + YD.^2 - a2^2 + 2*a2*a3D - a3D^2)).^(1/2)),(XD.^2 + 2*XD*a3D + YD.^2 - a2^2 + a3D^2));
-
-for i=1:length(th2_2);
-    th2=th2_2(i);
-    th5=th5_2(i);
-    Cx_1(i)=(a2^3*cos(th2) + a5^3*cos(th5) - a2*a3^2*cos(th2) + a2*a4^2*cos(th2) + a3^2*a5*cos(th5) - a4^2*a5*cos(th5) - a2*sin(th2)*(2*a2^2*a3^2 - a3^4 - a4^4 - a5^4 - a2^4 + 2*a2^2*a4^2 - 4*a2^2*a5^2 + 2*a3^2*a4^2 + 2*a3^2*a5^2 + 2*a4^2*a5^2 - 2*a2^2*a5^2*cos(2*th2 - 2*th5) + 4*a2*a5^3*cos(th2 - th5) + 4*a2^3*a5*cos(th2 - th5) - 4*a2*a3^2*a5*cos(th2 - th5) - 4*a2*a4^2*a5*cos(th2 - th5))^(1/2) + a5*sin(th5)*(2*a2^2*a3^2 - a3^4 - a4^4 - a5^4 - a2^4 + 2*a2^2*a4^2 - 4*a2^2*a5^2 + 2*a3^2*a4^2 + 2*a3^2*a5^2 + 2*a4^2*a5^2 - 2*a2^2*a5^2*cos(2*th2 - 2*th5) + 4*a2*a5^3*cos(th2 - th5) + 4*a2^3*a5*cos(th2 - th5) - 4*a2*a3^2*a5*cos(th2 - th5) - 4*a2*a4^2*a5*cos(th2 - th5))^(1/2) - a2*a5^2*cos(th2 - 2*th5) - a2^2*a5*cos(2*th2 - th5))/(2*(a2^2 - 2*cos(th2 - th5)*a2*a5 + a5^2));
-    Cx_2(i)=(a2^3*cos(th2) + a5^3*cos(th5) - a2*a3^2*cos(th2) + a2*a4^2*cos(th2) + a3^2*a5*cos(th5) - a4^2*a5*cos(th5) + a2*sin(th2)*(2*a2^2*a3^2 - a3^4 - a4^4 - a5^4 - a2^4 + 2*a2^2*a4^2 - 4*a2^2*a5^2 + 2*a3^2*a4^2 + 2*a3^2*a5^2 + 2*a4^2*a5^2 - 2*a2^2*a5^2*cos(2*th2 - 2*th5) + 4*a2*a5^3*cos(th2 - th5) + 4*a2^3*a5*cos(th2 - th5) - 4*a2*a3^2*a5*cos(th2 - th5) - 4*a2*a4^2*a5*cos(th2 - th5))^(1/2) - a5*sin(th5)*(2*a2^2*a3^2 - a3^4 - a4^4 - a5^4 - a2^4 + 2*a2^2*a4^2 - 4*a2^2*a5^2 + 2*a3^2*a4^2 + 2*a3^2*a5^2 + 2*a4^2*a5^2 - 2*a2^2*a5^2*cos(2*th2 - 2*th5) + 4*a2*a5^3*cos(th2 - th5) + 4*a2^3*a5*cos(th2 - th5) - 4*a2*a3^2*a5*cos(th2 - th5) - 4*a2*a4^2*a5*cos(th2 - th5))^(1/2) - a2*a5^2*cos(th2 - 2*th5) - a2^2*a5*cos(2*th2 - th5))/(2*(a2^2 - 2*cos(th2 - th5)*a2*a5 + a5^2));
-    Cy_1(i)=(a2^3*sin(th2) + a5^3*sin(th5) + a2*cos(th2)*(2*a2^2*a3^2 - a3^4 - a4^4 - a5^4 - a2^4 + 2*a2^2*a4^2 - 4*a2^2*a5^2 + 2*a3^2*a4^2 + 2*a3^2*a5^2 + 2*a4^2*a5^2 - 2*a2^2*a5^2*cos(2*th2 - 2*th5) + 4*a2*a5^3*cos(th2 - th5) + 4*a2^3*a5*cos(th2 - th5) - 4*a2*a3^2*a5*cos(th2 - th5) - 4*a2*a4^2*a5*cos(th2 - th5))^(1/2) - a5*cos(th5)*(2*a2^2*a3^2 - a3^4 - a4^4 - a5^4 - a2^4 + 2*a2^2*a4^2 - 4*a2^2*a5^2 + 2*a3^2*a4^2 + 2*a3^2*a5^2 + 2*a4^2*a5^2 - 2*a2^2*a5^2*cos(2*th2 - 2*th5) + 4*a2*a5^3*cos(th2 - th5) + 4*a2^3*a5*cos(th2 - th5) - 4*a2*a3^2*a5*cos(th2 - th5) - 4*a2*a4^2*a5*cos(th2 - th5))^(1/2) - a2*a3^2*sin(th2) + a2*a4^2*sin(th2) + a3^2*a5*sin(th5) - a4^2*a5*sin(th5) + a2*a5^2*sin(th2 - 2*th5) - a2^2*a5*sin(2*th2 - th5))/(2*(a2^2 - 2*cos(th2 - th5)*a2*a5 + a5^2));
-    Cy_2(i)=(a2^3*sin(th2) + a5^3*sin(th5) - a2*cos(th2)*(2*a2^2*a3^2 - a3^4 - a4^4 - a5^4 - a2^4 + 2*a2^2*a4^2 - 4*a2^2*a5^2 + 2*a3^2*a4^2 + 2*a3^2*a5^2 + 2*a4^2*a5^2 - 2*a2^2*a5^2*cos(2*th2 - 2*th5) + 4*a2*a5^3*cos(th2 - th5) + 4*a2^3*a5*cos(th2 - th5) - 4*a2*a3^2*a5*cos(th2 - th5) - 4*a2*a4^2*a5*cos(th2 - th5))^(1/2) + a5*cos(th5)*(2*a2^2*a3^2 - a3^4 - a4^4 - a5^4 - a2^4 + 2*a2^2*a4^2 - 4*a2^2*a5^2 + 2*a3^2*a4^2 + 2*a3^2*a5^2 + 2*a4^2*a5^2 - 2*a2^2*a5^2*cos(2*th2 - 2*th5) + 4*a2*a5^3*cos(th2 - th5) + 4*a2^3*a5*cos(th2 - th5) - 4*a2*a3^2*a5*cos(th2 - th5) - 4*a2*a4^2*a5*cos(th2 - th5))^(1/2) - a2*a3^2*sin(th2) + a2*a4^2*sin(th2) + a3^2*a5*sin(th5) - a4^2*a5*sin(th5) + a2*a5^2*sin(th2 - 2*th5) - a2^2*a5*sin(2*th2 - th5))/(2*(a2^2 - 2*cos(th2 - th5)*a2*a5 + a5^2));
-
-    Dx_1(i)=Cx_1(i)+a32*(Cx_1(i)-a2*cos(th2))/a3;
-    Dx_2(i)=Cx_2(i)+a32*(Cx_2(i)-a2*cos(th2))/a3;
-    Dy_1(i)=Cy_1(i)+a32*(Cy_1(i)-a2*sin(th2))/a3;
-    Dy_2(i)=Cy_2(i)+a32*(Cy_2(i)-a2*sin(th2))/a3;
-
-    Dx=Dx_2(i);
-    Dy=Dy_2(i);
-end
-
-Vyd=0.15*2*pi*(1/5)*cos(2*pi*(1/5)*t);
-Vxd=0*ones(1,length(t));
-
-for j=1:length(t);
-    th_D=inv([-a2*sin(th2_2(1,j)) -a3D*sin(th5_2(1,j));a2*cos(th2_2(1,j)) a3D*cos(th5_2(1,j))])*[Vyd(j);Vxd(j)];
-    dth2_d(j)=th_D(1);
-    dth5_d(j)=th_D(2);
-end
-
-th2_sim=th2_2;
-th5_sim=th5_2;
-XD_sim=XD;
-YD_sim=YD;
-
-save data_sim_control th2_sim th5_sim XD_sim YD_sim
-
-%% Simulation with forward kinematics (Cx and Cy calculated as a1=0)
-clear all; clc;
-a1=0;
-a2=0.2; a3=0.35; a4=a2; a5=a3; a32=a3; a3D=2*a3;
-TH2=(65:5:130)*pi/180;
-TH5=(45:5:110)*pi/180;
-
-for i=1:length(TH2);
-    th2=TH2(i);
-    th5=TH5(i);
-    Cx_1(i)=(a2^3*cos(th2) + a5^3*cos(th5) - a2*a3^2*cos(th2) + a2*a4^2*cos(th2) + a3^2*a5*cos(th5) - a4^2*a5*cos(th5) - a2*sin(th2)*(2*a2^2*a3^2 - a3^4 - a4^4 - a5^4 - a2^4 + 2*a2^2*a4^2 - 4*a2^2*a5^2 + 2*a3^2*a4^2 + 2*a3^2*a5^2 + 2*a4^2*a5^2 - 2*a2^2*a5^2*cos(2*th2 - 2*th5) + 4*a2*a5^3*cos(th2 - th5) + 4*a2^3*a5*cos(th2 - th5) - 4*a2*a3^2*a5*cos(th2 - th5) - 4*a2*a4^2*a5*cos(th2 - th5))^(1/2) + a5*sin(th5)*(2*a2^2*a3^2 - a3^4 - a4^4 - a5^4 - a2^4 + 2*a2^2*a4^2 - 4*a2^2*a5^2 + 2*a3^2*a4^2 + 2*a3^2*a5^2 + 2*a4^2*a5^2 - 2*a2^2*a5^2*cos(2*th2 - 2*th5) + 4*a2*a5^3*cos(th2 - th5) + 4*a2^3*a5*cos(th2 - th5) - 4*a2*a3^2*a5*cos(th2 - th5) - 4*a2*a4^2*a5*cos(th2 - th5))^(1/2) - a2*a5^2*cos(th2 - 2*th5) - a2^2*a5*cos(2*th2 - th5))/(2*(a2^2 - 2*cos(th2 - th5)*a2*a5 + a5^2));
-    Cx_2(i)=(a2^3*cos(th2) + a5^3*cos(th5) - a2*a3^2*cos(th2) + a2*a4^2*cos(th2) + a3^2*a5*cos(th5) - a4^2*a5*cos(th5) + a2*sin(th2)*(2*a2^2*a3^2 - a3^4 - a4^4 - a5^4 - a2^4 + 2*a2^2*a4^2 - 4*a2^2*a5^2 + 2*a3^2*a4^2 + 2*a3^2*a5^2 + 2*a4^2*a5^2 - 2*a2^2*a5^2*cos(2*th2 - 2*th5) + 4*a2*a5^3*cos(th2 - th5) + 4*a2^3*a5*cos(th2 - th5) - 4*a2*a3^2*a5*cos(th2 - th5) - 4*a2*a4^2*a5*cos(th2 - th5))^(1/2) - a5*sin(th5)*(2*a2^2*a3^2 - a3^4 - a4^4 - a5^4 - a2^4 + 2*a2^2*a4^2 - 4*a2^2*a5^2 + 2*a3^2*a4^2 + 2*a3^2*a5^2 + 2*a4^2*a5^2 - 2*a2^2*a5^2*cos(2*th2 - 2*th5) + 4*a2*a5^3*cos(th2 - th5) + 4*a2^3*a5*cos(th2 - th5) - 4*a2*a3^2*a5*cos(th2 - th5) - 4*a2*a4^2*a5*cos(th2 - th5))^(1/2) - a2*a5^2*cos(th2 - 2*th5) - a2^2*a5*cos(2*th2 - th5))/(2*(a2^2 - 2*cos(th2 - th5)*a2*a5 + a5^2));
-    Cy_1(i)=(a2^3*sin(th2) + a5^3*sin(th5) + a2*cos(th2)*(2*a2^2*a3^2 - a3^4 - a4^4 - a5^4 - a2^4 + 2*a2^2*a4^2 - 4*a2^2*a5^2 + 2*a3^2*a4^2 + 2*a3^2*a5^2 + 2*a4^2*a5^2 - 2*a2^2*a5^2*cos(2*th2 - 2*th5) + 4*a2*a5^3*cos(th2 - th5) + 4*a2^3*a5*cos(th2 - th5) - 4*a2*a3^2*a5*cos(th2 - th5) - 4*a2*a4^2*a5*cos(th2 - th5))^(1/2) - a5*cos(th5)*(2*a2^2*a3^2 - a3^4 - a4^4 - a5^4 - a2^4 + 2*a2^2*a4^2 - 4*a2^2*a5^2 + 2*a3^2*a4^2 + 2*a3^2*a5^2 + 2*a4^2*a5^2 - 2*a2^2*a5^2*cos(2*th2 - 2*th5) + 4*a2*a5^3*cos(th2 - th5) + 4*a2^3*a5*cos(th2 - th5) - 4*a2*a3^2*a5*cos(th2 - th5) - 4*a2*a4^2*a5*cos(th2 - th5))^(1/2) - a2*a3^2*sin(th2) + a2*a4^2*sin(th2) + a3^2*a5*sin(th5) - a4^2*a5*sin(th5) + a2*a5^2*sin(th2 - 2*th5) - a2^2*a5*sin(2*th2 - th5))/(2*(a2^2 - 2*cos(th2 - th5)*a2*a5 + a5^2));
-    Cy_2(i)=(a2^3*sin(th2) + a5^3*sin(th5) - a2*cos(th2)*(2*a2^2*a3^2 - a3^4 - a4^4 - a5^4 - a2^4 + 2*a2^2*a4^2 - 4*a2^2*a5^2 + 2*a3^2*a4^2 + 2*a3^2*a5^2 + 2*a4^2*a5^2 - 2*a2^2*a5^2*cos(2*th2 - 2*th5) + 4*a2*a5^3*cos(th2 - th5) + 4*a2^3*a5*cos(th2 - th5) - 4*a2*a3^2*a5*cos(th2 - th5) - 4*a2*a4^2*a5*cos(th2 - th5))^(1/2) + a5*cos(th5)*(2*a2^2*a3^2 - a3^4 - a4^4 - a5^4 - a2^4 + 2*a2^2*a4^2 - 4*a2^2*a5^2 + 2*a3^2*a4^2 + 2*a3^2*a5^2 + 2*a4^2*a5^2 - 2*a2^2*a5^2*cos(2*th2 - 2*th5) + 4*a2*a5^3*cos(th2 - th5) + 4*a2^3*a5*cos(th2 - th5) - 4*a2*a3^2*a5*cos(th2 - th5) - 4*a2*a4^2*a5*cos(th2 - th5))^(1/2) - a2*a3^2*sin(th2) + a2*a4^2*sin(th2) + a3^2*a5*sin(th5) - a4^2*a5*sin(th5) + a2*a5^2*sin(th2 - 2*th5) - a2^2*a5*sin(2*th2 - th5))/(2*(a2^2 - 2*cos(th2 - th5)*a2*a5 + a5^2));
-
-    Dx_1(i)=Cx_1(i)+a32*(Cx_1(i)-a2*cos(th2))/a3;
-    Dx_2(i)=Cx_2(i)+a32*(Cx_2(i)-a2*cos(th2))/a3;
-    Dy_1(i)=Cy_1(i)+a32*(Cy_1(i)-a2*sin(th2))/a3;
-    Dy_2(i)=Cy_2(i)+a32*(Cy_2(i)-a2*sin(th2))/a3;
-
-    Dx=Dx_2(i);
-    Dy=Dy_2(i);
+    % Display the angles
+    fprintf('Corner (%f, %f): Shoulder 1 = %f degrees, Shoulder 2 = %f degrees\n', x, y, rad2deg(shoulder1), rad2deg(shoulder2));
 end
