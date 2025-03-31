64ae0ef52f53d263933ace86b504ea22f2effa87
Modska <122678542+Modska@users.noreply.github.com>
2025-03-31 11:20:07 +0200

    Cercle

--- Panto_V2.m
+++ Panto_V2.m
@@ -1,4 +1,4 @@
-mdl = "FiveBar";
+mdl = "panto_v3";
 load_system(mdl);
 fk = simscape.multibody.KinematicsSolver(mdl);
 base = mdl + "/World Frame/W";
@@ -7,13 +7,21 @@ addFrameVariables(fk,"Pen","Translation", base, follower);
 
 % Specify the center and radius of the circle
 center_x = 0; % m
-center_y = 2.5; % m
+center_y = 2; % m
 radius = 0.15; % m
 th = 0:pi/50:2*pi; % radians
 
 % Save the x and y coordinates of the circle for the IK analyses
 coordinates_x = radius * cos(th) + center_x;
 coordinates_y = radius * sin(th) + center_y;
+
+% Plot the circle
+plot(coordinates_x,coordinates_y,"r","LineWidth",2)
+axis equal
+axis([-3 3 -3 3]);
+ylabel("Length (m)")
+xlabel("Width (m)")
+hold off
 
 ik = simscape.multibody.KinematicsSolver(mdl);
 
@@ -34,14 +42,26 @@ addInitialGuessVariables(ik,guessIds);
 
 guesses = [50 -50 45 135]; % Assign initial guesses for passive and active joints
 N = length(coordinates_x);
+
+% Définir les paramètres
+start_value = 0;
+end_value = 1; % Par exemple, si vous voulez que le vecteur aille jusqu'à 1
+num_points = 51; % Nombre total de points (par exemple, pour un incrément de 0.02)
+
+% Créer le vecteur breakpoint
+breakpoint = linspace(start_value, end_value, num_points);
+
+% Afficher le vecteur
+disp(breakpoint);
+
 index_in_ik = 1;
 ik_data = zeros(N,length(outputIds)); % Allocate memory for the variable
 for ind = 1:N
     targets = [coordinates_x(ind), coordinates_y(ind)]; % Use the x and y coordinates of the prescribed circle for the inverse kinematics problem
     [outputVec_ik,statusFlag_ik] = solve(ik,targets, guesses);
     if statusFlag_ik == 1 
-        viewSolution(ik) % View the solution in the Kinematics Solver Viewer
-        ik_data(index_in_ik,1:length(outputIds)) = outputVec_ik % Save the rotations of the motors
+        viewSolution(ik); % View the solution in the Kinematics Solver Viewer
+        ik_data(index_in_ik,1:length(outputIds)) = outputVec_ik; % Save the rotations of the motors
         index_in_ik=index_in_ik+1;
         guesses = [outputVec_ik(3) outputVec_ik(4) outputVec_ik(1) outputVec_ik(2)]; % Update next guess with last solution
     else
